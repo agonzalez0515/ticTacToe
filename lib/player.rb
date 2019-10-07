df3bf286f6a36119b9aa75691bf49fc634e8120d
Angelica Gonzalez <angelica@8thlight.com>
2019-10-07 13:23:17 -0700

    Create player subclasses and use minimax

--- lib/player.rb
+++ lib/player.rb
@@ -1,7 +1,42 @@
-class Player
-    attr_accessor :token
+require_relative 'minimax'
 
-    def initialize(token)
-        @token = token
+class Player
+    attr_reader :token
+
+    def initialize(args)
+        @token = args[:token]
+        @player_console = args[:console]
+    end
+
+    def choose_move
+        raise NotImplementedError
+    end
+end
+
+class HumanPlayer < Player
+    TO_INDEX = 1
+
+    def choose_move(board, opponent)
+        @player_console.get_player_input - TO_INDEX
+    end
+end
+
+
+class ComputerPlayer < Player
+    include Minimax
+    
+    def choose_move(board, opponent)
+        call_minimax(board, opponent)
+    end
+    
+    private
+    def call_minimax(board, opponent)
+        args = {
+            board: board,
+            marker: @token,
+            opponent: opponent,
+            empty_marker: ""
+        }
+        Minimax.best_move(args)
     end
 end
--- spec/player_spec.rb
+++ spec/player_spec.rb
@@ -1,8 +1,31 @@
 require 'player'
+require 'player_console'
 
 describe Player do
     it "creates a player with a value" do
-        player = Player.new('Angie')
+        player = Player.new({token: 'Angie'})
+        
         expect(player.token).to eq("Angie")
+    end   
+end
+
+describe HumanPlayer do 
+    it "gets input from player and converts it to index value" do
+        console = PlayerConsole.new
+        player = HumanPlayer.new({token: "X", console: console})
+        allow(console).to receive(:get_player_input) { 2 }
+        board = ["X","X","","","","","","",""]
+        
+        expect(player.choose_move(board, "X")).to eq(1)
+    end
+end
+
+describe ComputerPlayer do
+    it "returns a move" do
+        console = PlayerConsole.new
+        player = ComputerPlayer.new({token: "X", console: console})
+        board = ["X","X","","","","","","",""]
+
+        expect(player.choose_move(board,"O")).to eq(2)
     end
 end
