59fb7e7f37413d66c6ff21dd89be102704d57ed8
Angelica Gonzalez <angelica@8thlight.com>
2019-10-08 18:45:11 -0700

    Make methods private to simplify TTT class

--- lib/ttt.rb
+++ lib/ttt.rb
@@ -6,28 +6,58 @@ require_relative 'win'
 require_relative 'board'
 
 class TicTacToe
+    CURRENT = 0
+    OPPONENT = 1
 
     def initialize (args)
-        @board = Board.new() 
-        @players = args[:players]
+        @board = args[:board]
+        @players = []
         @player_console = args[:console]
-        @current_player = @players[0]
+        @current_player = @players[CURRENT]
         @win = args[:win]
     end
 
+    def new_game
+        start_game
+        play
+        end_game
+    end
+
+    private
     def start_game
         @player_console.print_starting_message
+        set_game_type(@player_console.get_game_type_response)
+        set_current_player
+    end
 
-        until @win.game_over?(@board.board) do
-            play
+    def end_game
+        if @win.game_over?(@board.board)
+            @win.print_game_over_message(@board.board, @players[OPPONENT].token)
         end
     end
 
+    def set_game_type(game_type)
+        human_player_1 = HumanPlayer.new({board: @board, console: @player_console, token: "X"})
+        human_player_2 = HumanPlayer.new({board: @board, console: @player_console, token: "O"})
+        computer_player = ComputerPlayer.new({board: @board, console: @player_console, token: "O"})
+
+        if game_type == 1
+            @players = [human_player_1, computer_player]
+        else
+            @players = [human_player_1, human_player_2]
+        end
+    end
+
+    def set_current_player
+        @current_player = @players[CURRENT]
+    end
 
     def play
-        @player_console.print_board(@board.board)
-        @player_console.print_instructions_message
-        player_turn
+        until @win.game_over?(@board.board) do
+            @player_console.print_board(@board.board)
+            @current_player.print_instructions_message
+            player_turn
+        end
     end
 
     def player_turn
@@ -40,9 +70,7 @@ class TicTacToe
             @player_console.print_invalid_input_message
         end
     end
-
     
-    private
     def get_player_input
         @current_player.choose_move
     end
@@ -51,9 +79,8 @@ class TicTacToe
         @board.board[input] == "" && [0,1,2,3,4,5,6,7,8].include?(input)
     end
         
-
     def switch_player
-        @players.rotate!
+        @current_player = @players.rotate![0]
     end
 end
 
--- spec/ttt_spec.rb
+++ spec/ttt_spec.rb
@@ -3,17 +3,14 @@ require 'pp'
 
 describe TicTacToe do
     let(:console) { PlayerConsole.new }
+    let(:board)   { Board.new }
     let(:win)     { Win.new(console: console) }
-    let(:players) {[HumanPlayer.new({token: "X", console: console}), ComputerPlayer.new({token: "O", console: console})]}
-    let(:game)    { TicTacToe.new({console: console, win: win, players: players}) }
+    let(:game)    { TicTacToe.new({console: console, win: win, board: board})}
     
-    
-    context '#player_turn' do
-        it "switches players if the token is placed successfully" do 
-            allow(console).to receive(:get_player_input) { 1 }
-            players = game.player_turn
-            
-            expect(players[0].token).to eq("O")
+    context "#new_game" do
+        it "creates a new game" do
+            expect(game).to be_an_instance_of(TicTacToe)
         end
     end
+    
 end
